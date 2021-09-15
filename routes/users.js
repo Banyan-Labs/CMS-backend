e08f877eb729f84e6be6ee56ce48a4dfd058e5ae
StephanieKirshner <88007877+StephanieKirshner@users.noreply.github.com>
2021-09-15 09:15:25 -0700

    created middleware to get one user

--- routes/users.js
+++ routes/users.js
@@ -13,11 +13,31 @@ router.get('/', async (req, res) => {
 })
 
 // Get one user
+router.get('/:id', getUsers, (req, res) => {
+    res.json(res.users)
+});
 
 // Post a user
 
 // Patch a user
 
 // Delete a user
+
+// Middleware
+async function getUsers(req, res, next) {
+    let users
+    try {
+        users = await User.findById(req.params.id)
+        if (users == null) {
+            return res.status(404).json({ message: 'Cannot find user'})
+    }
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+
+    res.users = users
+    next()
+};
+
 
 module.exports = router
