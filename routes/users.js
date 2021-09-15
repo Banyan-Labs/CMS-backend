b551845dcb53dddf95571b0cd4985c8f22828f3d
StephanieKirshner <88007877+StephanieKirshner@users.noreply.github.com>
2021-09-15 09:00:42 -0700

    created post route for users

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,21 @@ const User = require('../models/user')
 // Get one user
 
 // Post a user
+router.post('/', async (req, res) => {
+    const UsersPost = new User({
+        username: req.body.username,
+        password: req.body.password,
+        email: req.body.email,
+    })
+
+    try {
+        const newUser = await UsersPost.save()
+        res.status(201).json(newUser)
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+})
+
 
 // Patch a user
 
