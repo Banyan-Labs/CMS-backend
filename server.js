de331184b1112cacb84e2eb2a4ed9d0e07f26178
jdickerson-banyan <75698869+jdickerson-banyan@users.noreply.github.com>
2021-09-27 09:58:20 -0700

    fixed start script and added cors

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require("cors");
+app.use(cors());
 
 mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
@@ -10,6 +12,7 @@ db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
 app.use(express.json())
+app.use(cors());
 
 const studiesRouter = require('./routes/caseStudies')
 app.use('/cases', studiesRouter)
